a830cfa7ec78d3dc024a674dc04f8c67605e2f40
Aieat Assam <Aieat.Assam@epicor.com>
2019-02-05 14:47:50 +0000

    Updated function to utilise Azure Device Client SDK for property update, since registry side can only handle desired properties, not reported ones.

--- IoTEventToTwinProperties/IoTHubToTwinPropertiesFunction.cs
+++ IoTEventToTwinProperties/IoTHubToTwinPropertiesFunction.cs
@@ -17,13 +17,15 @@ using System;
 using System.Data;
 using System.Linq;
 using System.Threading;
+using Microsoft.Azure.Devices.Client;
+using IotHubConnectionStringBuilder = Microsoft.Azure.Devices.IotHubConnectionStringBuilder;
 
 namespace IoTEventToTwinProperties
 {
     public static class IoTHubToTwinPropertiesFunction
     {
-        private static HttpClient client = new HttpClient();
         private static RegistryManager _manager;
+        private static DeviceClient _client;
 
         [FunctionName("IoTHubToTwinPropertiesFunction")]
         public static async Task RunAsync(
@@ -50,6 +52,7 @@ namespace IoTEventToTwinProperties
             var twin = await _manager.GetTwinAsync(deviceId, token);
 
             // merge properties
+            twin.Properties.Reported.ClearMetadata();
             var reported = JObject.Parse(twin.Properties.Reported.ToJson());
 
             var eventData = JObject.Parse(Encoding.UTF8.GetString(message.Body.ToArray()));
@@ -71,18 +74,61 @@ namespace IoTEventToTwinProperties
                 // need to update
                 try
                 {
-                    twin.Properties.Reported = new TwinCollection(updatedProps.ToString());
-                    await _manager.UpdateTwinAsync(deviceId, twin, twin.ETag, token);
+                    var device = await _manager.GetDeviceAsync(deviceId, token);
+                    var builder = IotHubConnectionStringBuilder.Create(connectionString);
+
+                    var deviceClient = DeviceClient.Create(builder.HostName,
+                        new DeviceAuthenticationWithRegistrySymmetricKey(deviceId,
+                            device.Authentication.SymmetricKey.PrimaryKey));
+
+                    await deviceClient.UpdateReportedPropertiesAsync(new TwinCollection(updatedProps.ToString()), token);
+                    // call below can only be used for desired properties! So we cannot use it even though it is tempting to do so!
+                    //await _manager.UpdateTwinAsync(deviceId, twin, twin.ETag, token);
                     log.LogInformation($"Digital twin for {deviceId} updated from {reported} to {updatedProps}");
                 }
                 catch (Exception e)
                 {
-                    log.LogError($"Error updating digital twin of {deviceId} to {twin.ToJson()}: {e}");
+                    log.LogError($"Error updating reported properties of digital twin of {deviceId} to {updatedProps}: {e}");
                     throw;
                 }
                 
             }
             
+        }
+
+
+
+        /// <summary>
+        /// Gets the parameters from an IoT Hub Connection String (decrypted)
+        /// </summary>
+        /// <param name="connectionString"></param>
+        /// <returns></returns>
+        public static Dictionary<string, string> GetIoTHubConnectionParameters(string connectionString)
+        {
+            var parameters = new Dictionary<string, string>();
+            try
+            {
+                parameters = connectionString.Split(';')
+                    .ToDictionary(p => p.Split('=')[0],
+                        p => p.Split('=')[1],
+                        StringComparer.InvariantCultureIgnoreCase);
+            }
+            catch
+            {
+                throw new ArgumentException("Connection string is malformed.");
+            }
+            return parameters;
+        }
+
+        public static string GetHubName(string connectionString)
+        {
+            var parameters = GetIoTHubConnectionParameters(connectionString);
+            if (parameters.ContainsKey("HostName") == false)
+            {
+                throw new Exception("Hostname not found in connection string.");
+            }
+            var hostName = parameters["HostName"];
+            return hostName.Split('.')[0];
         }
 
 
